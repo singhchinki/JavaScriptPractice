97913b6592a64edb7879a536a45e3a2e2901b794
chinki kumari singh <singhchinkikumari.com>
2022-08-16 22:35:10 +0530

    [Chinki] Use  of Hosting

--- JavaConcept.js
+++ JavaConcept.js
@@ -22,13 +22,47 @@
 //  abc(5,6);
 
 //Arrow Function
-let greet = x => console.log(x);
-greet('Arrow Expression');
+// let greet = x => console.log(x);
+// greet('Arrow Expression');
 //Multiple Arrow Function
-let sum = (a, b) => {
-    let result = a + b;
-    return result;
-}
+// let sum = (a, b) => {
+//     let result = a + b;
+//     return result;
+// }
 
-let result1 = sum(5,7);
-console.log(result1);
+// let result1 = sum(5,7);
+// console.log(result1);
+
+// Synchronusly
+// var sychVsAsych
+
+// console.log('Hi!');
+// console.log('Good Morning');
+// console.log('How are you?');
+
+// console.log('hi');
+// setTimeout[() => {
+//     console.log('hello');
+// }, 5000];
+
+// console.log('I am good');
+
+// hosting variable with var, let, const
+// program to display value
+// var a = 4;
+
+// function greet() {
+//     b = 'hello';
+//     console.log(b); // hello
+//     var b;
+// }
+
+// greet(); // hello
+// console.log(b);
+
+// program to print the text
+greet();
+
+let greet = function() {
+    console.log('Hi, there.');
+}
