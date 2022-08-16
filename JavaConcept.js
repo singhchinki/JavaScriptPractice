8868c7f918e7200ac4702dca6ff24c52565dfa55
chinki kumari singh <singhchinkikumari.com>
2022-08-16 21:06:13 +0530

    [Chinki] Use  of Function

---
+++ JavaConcept.js
@@ -0,0 +1,34 @@
+// How to use Funtion in JavaScript
+// function Add(a, b) 
+// {
+//     var a  = 10;
+//     var b = 20;
+//     let add = a + b;
+//    console.log(add);
+//    // return add;
+// }
+// Add();
+
+// function outSideTheFunction(name)
+// {
+//     console.log('Good Morning', name)
+// }
+// outSideTheFunction('smita')
+
+//anonymous function
+//  var abc =function (p1,p2) {
+//     console.log(p1 + p2)
+//  };
+//  abc(5,6);
+
+//Arrow Function
+let greet = x => console.log(x);
+greet('Arrow Expression');
+//Multiple Arrow Function
+let sum = (a, b) => {
+    let result = a + b;
+    return result;
+}
+
+let result1 = sum(5,7);
+console.log(result1);
