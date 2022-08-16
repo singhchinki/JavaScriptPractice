306350280bfc7c6e4916deb5fb35edfe1ed6e83c
chinki kumari singh <singhchinkikumari.com>
2022-08-16 21:52:41 +0530

    [Chinki] Use  of Synchronus and Asynchronus

--- JavaConcept.js
+++ JavaConcept.js
@@ -22,13 +22,28 @@
 //  abc(5,6);
 
 //Arrow Function
-let greet = x => console.log(x);
-greet('Arrow Expression');
+// let greet = x => console.log(x);
+// greet('Arrow Expression');
 //Multiple Arrow Function
-let sum = (a, b) => {
-    let result = a + b;
-    return result;
-}
+// let sum = (a, b) => {
+//     let result = a + b;
+//     return result;
+// }
 
-let result1 = sum(5,7);
-console.log(result1);
+// let result1 = sum(5,7);
+// console.log(result1);
+
+// Synchronusly
+var sychVsAsych
+
+// console.log('Hi!');
+// console.log('Good Morning');
+// console.log('How are you?');
+
+console.log('hi');
+setTimeout[() => {
+    console.log('hello');
+}, 5000];
+
+console.log('I am good');
+ 
